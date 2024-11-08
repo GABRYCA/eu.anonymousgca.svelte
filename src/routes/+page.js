7dcf3bb12505a03a37f597fa4f2675ade3c8cc95
GABRYCA <39743848+GABRYCA@users.noreply.github.com>
2024-11-08 16:46:33 +0100

    Changelogs: - Troubleshooting. - Minor changes.

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,9 @@
 export const load = async ({}) => {
     return {
         title: 'Home - AnonymousGCA',
+        description: 'AnonymousGCA - Web Developer, Full-Stack Developer, University Student, SvelteKit enthusiast, Soccer-Robocup programmer',
+        index: true,
+        keywords: 'AnonymousGCA, Developer, Full-Stack, Frontend, Backend, Designer, Web Development, Svelte, SvelteKit, University Student'
+
     }
 }
