fdeac1803470303ce7938c492f85545c13566892
GABRYCA <39743848+GABRYCA@users.noreply.github.com>
2024-11-11 23:19:18 +0100

    Changelogs: - Minor changes.

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -4,7 +4,7 @@ export const load = async ({ url }) => {
         description: 'AnonymousGCA - Web Developer, Full-Stack Developer, University Student, SvelteKit enthusiast, Soccer-Robocup programmer',
         siteName: 'AnonymousGCA - Official',
         imageURL: `${url.origin}/favicon-512.webp`,
-        logo: `${url.origin}https://anonymousgca.eu/favicon-512.webp`,
+        logo: `${url.origin}/favicon-512.webp`,
         author: 'AnonymousGCA',
         name: 'AnonymousGCA',
         twitter: true,
