f55690a2664833446561d0d0251254a9fb050554
GABRYCA <39743848+GABRYCA@users.noreply.github.com>
2024-11-13 11:31:03 +0100

    Changelogs: - Removed double re-declaration of meta on homepage. - Minor changes.

--- src/routes/+page.js
+++
@@ -1,7 +0,0 @@
-export const load = async ({}) => {
-    return {
-        title: 'Home - AnonymousGCA',
-        description: 'AnonymousGCA - Web Developer, Full-Stack Developer, University Student, SvelteKit enthusiast, Soccer-Robocup programmer',
-        keywords: 'AnonymousGCA, Developer, Full-Stack, Frontend, Backend, Designer, Web Development, Svelte, SvelteKit, University Student'
-    }
-}
