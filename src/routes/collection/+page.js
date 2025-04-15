8220c352fc0f15a3c88bf3bd3c4c1681255daa22
anonymousgca <anonymousgca@anonymousgca.eu>
2025-04-16 00:14:39 +0200

    Changelogs: - Enabled prerendering on a few selected pages. - Minor changes.

--- src/routes/collection/+page.js
+++ src/routes/collection/+page.js
@@ -1,3 +1,5 @@
+export const prerender = true;
+
 export const load = async ({}) => {
     return {
         title: 'Collection - AnonymousGCA',
--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,5 +1,7 @@
 import * as sitemap from 'super-sitemap';
 
+export const prerender = false;
+
 export const GET = async () => {
     return await sitemap.response({
         origin: 'https://anonymousgca.eu',
